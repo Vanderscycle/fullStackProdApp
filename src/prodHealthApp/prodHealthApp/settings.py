99cdcc18acfac93c416bb0c98a451b58946acc9b
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-01-16 16:43:22 -0800

    create root user and environment file. DB successfully connected to Postgres

--- src/prodHealthApp/prodHealthApp/settings.py
+++ src/prodHealthApp/prodHealthApp/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+DEBUG = env('DEBUG')
+POSTGRES_ADMIN_SECRET = env('POSTGRES_ADMIN_SECRET')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'prodHealthApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'productivityapp',
+        'USER': 'root',
+        'PASSWORD': POSTGRES_ADMIN_SECRET,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
